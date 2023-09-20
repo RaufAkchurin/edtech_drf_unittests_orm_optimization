a27255a50e70ebc45b0c8bceb930ef37165d088b
rauf.akchurin <abdurauf.dev@gmail.com>
2023-09-20 19:00:51 +0500

    models first

--- HardCodeApp/admin.py
+++ HardCodeApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.admin.sites import site
+
+from HardCodeApp.models import Product, Access, Lesson, View
 
 # Register your models here.
+
+site.register(Product)
+site.register(Access)
+site.register(Lesson)
+site.register(View)
--- HardCodeApp/models.py
+++ HardCodeApp/models.py
@@ -1,3 +1,52 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=10)
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="products",
+    )
+
+
+class Access(models.Model):
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="accesses",
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="accesses",
+    )
+
+
+class Lesson(models.Model):
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="lessons",
+    )
+    name = models.CharField(max_length=10)
+    link = models.URLField()
+    duration = models.IntegerField()
+
+
+class View(models.Model):
+    lesson = models.ForeignKey(
+        Lesson,
+        on_delete=models.CASCADE,
+        related_name="views",
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="views",
+    )
+    finished = models.BooleanField()
